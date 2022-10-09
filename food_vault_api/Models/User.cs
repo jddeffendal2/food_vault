b0c9d7cb233264744d2678b60d23549002b233e6
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2022-10-09 14:08:29 -0400

    Create Recipe Model

--- food_vault_api/Models/User.cs
+++ food_vault_api/Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
         public DateTime CreatedDate { get; set; }
