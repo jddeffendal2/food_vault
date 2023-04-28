389d4aac503f6a3b2b2f9e61713778f730205baf
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-04-28 16:58:28 -0400

    add recipeCreation link to homePage

--- FoodVault_Web/src/views/HomeView.vue
+++ FoodVault_Web/src/views/HomeView.vue
@@ -3,5 +3,7 @@ import { onMounted } from "vue";
 </script>
 
 <template>
-  HOME PAGE!!!
+  <div><h2>Home Page!</h2></div>
+
+  <p>Create your first Recipe: <RouterLink to="/AddRecipeOptions"> <button>Create Recipe</button></RouterLink></p>
 </template>
