377a792c39043f43b3caebea1b9e0f59a577ada0
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-05-11 14:32:25 -0400

    add basic recipe creations in web folder

--- FoodVault_Web/src/views/AddRecipeOptionsView.vue
+++ FoodVault_Web/src/views/AddRecipeOptionsView.vue
@@ -5,6 +5,9 @@
     <label for="recipeName">Name of Recipe: </label>
     <input type="text" id="recipeName" v-model="recipeName" />
     <br /><br />
+    <label for="description">Description of Recipe: </label>
+    <input type="text" id="description" v-model="description" />
+    <br/><br/>
     <label for="ingredients"><b>Ingredients:</b></label>
     <div v-for="ingredient in ingredients" :key="ingredient">
       <span>{{ ingredient.id }}: </span>
@@ -19,17 +22,34 @@
     <br /><br />
     <div id="extraInstructions"></div>
     <br /><br />
+    <button id="addIngredient" @click="addIngredient">Add Another Ingredient</button>
     <button id="addInstruction" @click="addInstruction">Add Another Instruction</button>
     <br /><br />
-    <button id="saveRecipe" @click="saveRecipe">Save Recipe</button>
+    <button id="saveRecipe" @click="createRecipe">Save Recipe</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useAccountStore } from "../stores/accountStore";
+import { saveRecipe } from "../utils/request.js";
+
+const accountStore = useAccountStore();
 
 const ingredients = ref([]);
 const instructions = ref([]);
+
+var recipeName = ref("");
+var description = ref("");
+
+const createRecipe = function() {
+  const recipe = {
+    creator: accountStore.currentUserId,
+    name: recipeName.value, 
+    description: description.value
+  };
+  saveRecipe(recipe, (res) => console.log(res));
+}
 
 const addIngredient = function() {
   var newIngredient = {
