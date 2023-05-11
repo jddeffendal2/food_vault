4b32f1e6caa88dd2a6e59de6c2b7e24ea7023128
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-05-11 14:42:47 -0400

    recipe api work

---
+++ Backend/FoodVaultApi/Controllers/RecipeController.cs
@@ -0,0 +1,46 @@
+﻿using FoodVaultApi.DTO;
+using FoodVaultApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodVaultApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class RecipeController : ControllerBase
+    {
+        private readonly FoodVaultDbContext _context;
+        private IConfiguration _configuration;
+
+        public RecipeController(IConfiguration config, FoodVaultDbContext context)
+        {
+            _configuration = config;
+            _context = context;
+        }
+
+        [HttpPost("Create")]
+
+        public IActionResult Create(RecipeDTO recipeDto)
+        {
+            var recipe = new Recipe
+            {
+                Id = Guid.NewGuid().ToString().ToUpper(),
+                UserId = recipeDto.creator,
+                Name = recipeDto.name,
+                Description = recipeDto.description,
+                CreatedDate = DateTime.UtcNow,
+                UpdatedDate = DateTime.UtcNow,
+            };
+
+            _context.Recipes.Add(recipe);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ Backend/FoodVaultApi/DTO/RecipeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodVaultApi.DTO
+{
+    public class RecipeDTO
+    {
+        public string creator { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
