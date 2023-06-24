b37da030a5c6277353e5b16894653709e670f44b
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-06-24 12:28:29 -0400

    add basic notifications work

--- FoodVault_Web/src/App.vue
+++ FoodVault_Web/src/App.vue
@@ -4,10 +4,28 @@
       <h1 class="brand-name">Food Vault</h1>
     </RouterLink>
     <ul class="nav-bar">
+      <li><RouterLink to="/Groups">Recipes</RouterLink></li>
       <li><RouterLink to="/Groups">Groups</RouterLink></li>
       <li>Invitations</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li>
+        <div class="notification" @click="openNotifications">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+            </svg>
+          </span>
+          <span class="badge">9</span>
+        </div>
+      </li>
     </ul>
     <button v-if="accountStore.isLoggedIn" class="logout-button" type="button" @click="logOut">Log Out</button>
+    <NotificationsModal v-if="isNotificationsOpen" @close="closeNotificationsModal"></NotificationsModal>
   </header>
   <div>
     <RouterView />
@@ -15,11 +33,13 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useAccountStore } from "@/stores/accountStore.js";
 import router from "./router";
+import NotificationsModal from "@/components/NotificationsModal.vue";
 
 const accountStore = useAccountStore();
+const isNotificationsOpen = ref(false);
 
 onMounted(async () => {
   await accountStore.initialize();
@@ -31,6 +51,14 @@ onMounted(async () => {
 
 function logOut() {
   accountStore.logOut()
+}
+
+const openNotifications = function() {
+  isNotificationsOpen.value = true;
+}
+
+const closeNotificationsModal = function() {
+  isNotificationsOpen.value = false;
 }
 
 // Potential colors: #FFFEF2 background white
@@ -80,7 +108,6 @@ ul li {
 }
 
 .logout-button {
-  margin: auto auto auto 400px;
   min-width: 100px;
   max-width: 100px;
   min-height: 40px;
@@ -100,7 +127,29 @@ ul li {
     display: flex;
     flex-direction: row;
     align-items: center;
-    /* justify-content: space-between; */
   }
 }
+
+.notification {
+  text-decoration: none;
+  position: relative;
+}
+.notification:hover {
+  .badge{
+    background-color: red;
+  }
+  cursor: pointer;
+}
+
+.badge {
+  position: absolute;
+  top: -8px;
+  right: -7px;
+  padding: 0px 4px;
+  border-radius: 50%;
+  background-color: #043565;
+  color: white;
+  font-size: 10px;
+}
+
 </style>
---
+++ FoodVault_Web/src/components/NotificationsModal.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="modal-background">
+    <div class="modal">
+      <header class="modal-header">
+        <slot id="header" name="header"> You have 9 New Notifications </slot>
+        <button type="button" class="btn-close" @click="close">x</button>
+      </header>
+
+      <section class="modal-body">
+        <div>
+            You have been invited to join a group
+        </div>
+        <br />
+        <button type="button" class="close-button" @click="close">View Invitations</button>
+      </section>
+    </div>
+  </div>
+</template>
+<script setup>
+const emit = defineEmits(["close"]);
+
+const close = async function () {
+  emit("close");
+};
+</script>
+<style scoped>
+.modal-background {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  min-width: 500px;
+  max-width: 500px;
+  min-height: 270px;
+  max-height: 270px;
+  background: #ffffff;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  border-radius: 15px;
+}
+.modal-header {
+  border-radius: 15px;
+  padding: 15px;
+  display: flex;
+  background-color: #c7d6d5;
+  position: relative;
+  border-bottom: 1px solid #eeeeee;
+  color: #043565;
+  font-weight: bold;
+  justify-content: center;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+  text-align: center;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #043565;
+  background: transparent;
+}
+
+.close-button {
+  color: #043565;
+  background: #c7d6d5;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+
+.close-button:hover {
+  cursor: pointer;
+  box-shadow: 1px 1px 1px gray;
+}
+</style>
