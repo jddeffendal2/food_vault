497a6ed44cb05b4e1a28297ab4c1e0e5ddb7b683
jddeffendall <59580674+jddeffendall@users.noreply.github.com>
2024-09-27 16:29:35 -0400

    Add permissions to UserGroup

---
+++ Backend/FoodVaultApi/Migrations/20240927202549_CreateUserGroupPermissions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodVaultApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserGroupPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanAddRecipes",
+                table: "UserGroups",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CanAddUsers",
+                table: "UserGroups",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanAddRecipes",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "CanAddUsers",
+                table: "UserGroups");
+        }
+    }
+}
